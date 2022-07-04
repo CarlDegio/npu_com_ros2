7e151a0abb65b5ba74184d564afd22260520efea
CarlDegio <2536890272Carl@gmail.com>
2022-07-04 11:38:37 +0800

    应对数梅派算逆解太慢，将逆解迁移到nuc，将通信改为了只传输pwm数

--- include/pkg.hpp
+++ include/pkg.hpp
@@ -6,13 +6,14 @@
 #define NPU_COM_ROS2_NPU_COM_TCP_HPP
 struct Tcp_Pkg
 {
-    double posx=0;
-    double posy=15;
-    double posz=15;
-    double pitch=0;
-    double timelen=3000;
+    short pwm1=327;
+    short pwm2=998;
+    short pwm3=411;
+    short pwm4=414;
+    short timelen1=3000;
     short closepwm=200;
     short betapwm=500;
+    short timelen2=3000;
     int magic_num=997;
 };
 #endif //NPU_COM_ROS2_NPU_COM_TCP_HPP
--- src/topic_to_tcp.cpp
+++ src/topic_to_tcp.cpp
@@ -7,7 +7,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int16_multi_array.hpp"
-#include "std_msgs/msg/float64_multi_array.hpp"
+//#include "std_msgs/msg/float64_multi_array.hpp"
 //#include "npu_msg/msg/armpkg.hpp"
 //#include "npu_msg/msg/endset.hpp"
 #include "pkg.hpp"
@@ -20,15 +20,15 @@ public:
     Ros2Tcp()
             : Node("minimal_publisher")
     {
-        this->declare_parameter<std::string>("raspberry_ip", "192.168.149.62");
+        this->declare_parameter<std::string>("raspberry_ip", "192.168.149.54");
         this->declare_parameter<ushort>("ip_port", 10000);
 
         this->get_parameter("raspberry_ip",raspberry_ip);
         this->get_parameter("ip_port",ip_port);
-        pose_subscription_=this->create_subscription<std_msgs::msg::Float64MultiArray>(
-                "/arm_close_gamma", 10, std::bind(&Ros2Tcp::pos_callback, this, _1));
+        pose_subscription_=this->create_subscription<std_msgs::msg::Int16MultiArray>(
+                "/arm_pos_time", 10, std::bind(&Ros2Tcp::pos_callback, this, _1));
         PWM_subscription_ = this->create_subscription<std_msgs::msg::Int16MultiArray>(
-                "/close_gamma", 10, std::bind(&Ros2Tcp::PWM_callback, this, _1));
+                "/arm_close_gamma", 10, std::bind(&Ros2Tcp::PWM_callback, this, _1));
 
         tcp_pkg.magic_num=997;//magic num to check
 
@@ -36,7 +36,7 @@ public:
         server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         server_addr.sin_family = AF_INET;  //使用IPv4地址
         server_addr.sin_addr.s_addr = inet_addr(raspberry_ip.c_str());  //具体的IP地址
-        server_addr.sin_port = htons(10000);  //端口
+        server_addr.sin_port = htons(ip_port);  //端口
         bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
         listen(server_sock, 20);
@@ -55,21 +55,22 @@ public:
     }
 
 private:
-    void pos_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg) {
+    void pos_callback(const std_msgs::msg::Int16MultiArray::SharedPtr msg) {
         //RCLCPP_INFO(this->get_logger(), "sub pos");
-        tcp_pkg.posx=msg->data[0];
-        tcp_pkg.posy=msg->data[1];
-        tcp_pkg.posz=msg->data[2];
-        tcp_pkg.pitch=msg->data[3];
-        tcp_pkg.timelen=msg->data[4];
+        tcp_pkg.pwm1=msg->data[0];
+        tcp_pkg.pwm2=msg->data[1];
+        tcp_pkg.pwm3=msg->data[2];
+        tcp_pkg.pwm4=msg->data[3];
+        tcp_pkg.timelen1=msg->data[4];
     }
     void PWM_callback(const std_msgs::msg::Int16MultiArray::SharedPtr msg) {
-        RCLCPP_INFO(this->get_logger(), "sub pwm");
+        //RCLCPP_INFO(this->get_logger(), "sub pwm");
         tcp_pkg.closepwm=msg->data[0];
         tcp_pkg.betapwm=msg->data[1];
+        tcp_pkg.timelen2=msg->data[2];
         write(client_sock,(char *)&tcp_pkg,sizeof(Tcp_Pkg));//this faster, so pub heres
     }
-    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr pose_subscription_;
+    rclcpp::Subscription<std_msgs::msg::Int16MultiArray>::SharedPtr pose_subscription_;
     rclcpp::Subscription<std_msgs::msg::Int16MultiArray>::SharedPtr PWM_subscription_;
 
     int server_sock;
